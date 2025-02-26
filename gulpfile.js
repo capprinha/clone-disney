44f0639eb1041235599378721a63d167b8c6c31c
capprinha <gutoklein21@gmail.com>
2025-02-26 20:19:20 -0300

    configurando o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const imagemin = require('gulp-imagemin')
+const uglify = require('gulp-uglify')
+
+function scripts(){
+    return gulp.src('./source/scripts/main.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+}
 
 function compilaSass(){
     return gulp.src('./source/styles/*.scss')
@@ -19,4 +26,5 @@ function waTch(){
 }
 
 gulp.task('watch', waTch)
-exports.default = gulp.parallel(compilaSass, imagens)
+
+exports.default = gulp.parallel(compilaSass, imagens, scripts)
