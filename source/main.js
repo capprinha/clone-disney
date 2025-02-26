ec9ee6f2d37d26905f16d36a2d1884430b24f5e8
capprinha <gutoklein21@gmail.com>
2025-02-25 21:08:15 -0300

    criação de cabeçalho, faq, footer e dispositivos disponiveis

--- source/main.js
+++ source/main.js
@@ -1,8 +1,23 @@
 document.addEventListener('DOMContentLoaded', function(){
 
     const buttons = document.querySelectorAll('[data-tab-button]');
-    
+    const questions = document.querySelectorAll('[data-faq-question]')
 
+    const heroSection = document.querySelector('.hero')
+    const alturaHero = heroSection.clientHeight
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY  
+
+        if(posicaoAtual < alturaHero){
+            ocultaHeader();
+        }
+        else{
+            exibeHeader();
+        }
+    });
+    
+    // progrmação das abas
     for( let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener('click',function(botao){
             const abaAlvo = botao.target.dataset.tabButton;
@@ -10,10 +25,32 @@ document.addEventListener('DOMContentLoaded', function(){
             escondeTodasAbas();
             aba.classList.add('shows__list--is-active');
             removeBotaoAtivo();
-            botao.target.classList.add('shows__tabs__button--is-active')
+            botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    // accordion
+    for(let i = 0; i < questions.length; i++){
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 })
+
+function ocultaHeader(){
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden')
+}
+
+function exibeHeader(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden')
+}
+
+function abreOuFechaResposta(elemento){
+    const classe = 'faq__questions__item--is-open'
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo(){
     const buttons = document.querySelectorAll('[data-tab-button]');
